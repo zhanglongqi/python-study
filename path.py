87accf67b7aa3ce4e22630bff3c3d39b73dbc314
benhuan <benhuan@126.com>
2014-05-21 10:23:57 +0800

    test 1

--- path.py
+++ path.py
@@ -15,3 +15,4 @@ import inspect, os
 print inspect.getfile(inspect.currentframe()) # script filename (usually with path)
 print os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
 print inspect.getfile(os)
+print os.path.abspath(".")  #current path
