842bb8f506b91e01c90157135e70d7d4c1035044
longqi <longqi90@gmail.com>
2015-11-26 23:02:49 +0800

    how to get the output after subprocess call command

--- my_subprocess.py
+++ my_subprocess.py
@@ -16,3 +16,16 @@ import sys
 process1 = subprocess.Popen('ls -sh /home2', shell=True, stderr=subprocess.PIPE)
 process1.communicate()
 sys.stderr.write('Message to stderr, hi zhang\n')
+
+print('2_______________________________')
+process2 = subprocess.Popen('who', shell=True, stdout=subprocess.PIPE)
+
+out, err = process2.communicate()
+print(out, err)
+
+print('3_______________________________')
+
+if any(s.startswith(b'longqi ') for s in out.split(b'\n')):
+    print('LongQi is here')
+else:
+    print('He is not here, Find him somewhrere else')
